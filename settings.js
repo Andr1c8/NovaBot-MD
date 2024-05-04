a64c73a0ba90c94d59045ba6d01486bafb0ed809
Andr1c8 <165866763+Andr1c8@users.noreply.github.com>
2024-05-04 00:31:45 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.blockList = []
 
 //---------[ NOMBRE/INFO ]---------
 global.botname = "𝙰𝙻𝙸𝚂𝙰𝙱𝙾𝚃-𝙼𝙳"
-global.wm = '𝘈𝘓𝘐𝘚𝘈𝘉𝘖𝘛-𝘔𝘋 💫'
+global.wm = '𝘈𝘓𝘐𝘚𝘈𝘉𝘖𝘛-𝘔𝘋🌸'
 global.vs = '1.0.7'
 
 //---------[ FECHA/IDIOMAS ]---------
