bfcebe2c1a5e2b26d99d06bd6e506ef8df03e230
Andr1c8 <165866763+Andr1c8@users.noreply.github.com>
2024-05-08 02:01:17 -0400

    Actualizar rpg.js

--- plugins/rpg.js
+++ plugins/rpg.js
@@ -281,6 +281,55 @@ const str = `╭╌「 *LEVEL UP 🎊* 」
 ${lenguaje.rpg.level7}`.trim()
 return m.reply(str)}}
 
+let crime = 500
+let diamante = 10
+const handler = async (m, {conn, usedPrefix, command, groupMetadata, participants, isPrems}) => {
+const date = global.db.data.users[m.sender].crime + 3600000; //3600000 = 1 hs
+if (new Date - global.db.data.users[m.sender].crime < 3600000) return m.reply(`Espera el ⏱️ : ${msToTime(date - new Date())}`)
+let randow
+if (m.isGroup) randow = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else randow = m.chat
+try {
+let ps = groupMetadata.participants.map(v => v.id)
+let randow = ps.getRandom()
+let users = global.db.data.users[randow]
+const exp = Math.floor(Math.random() * 9000)
+const diamond = Math.floor(Math.random() * 150)
+const money = Math.floor(Math.random() * 9000)
+let or = ['text', 'text2', 'text3', 'text4', 'text5']; 
+let media = or[Math.floor(Math.random() * 4)]
+global.db.data.users[m.sender].crime = new Date * 1;
+if (media === 'text') return m.reply(`《💸》${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
+if (media === 'text2') return m.reply(`《⏱️》${pickRandom(global.robmal)} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= crime) 
+if (media === 'text3') return m.reply(`《💸》*${pickRandom(global.robar)}*\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🌸 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
+if (media === 'text4') return m.reply(`《🚓》${pickRandom(global.robmal)}\n\n${diamond} 💎 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄\n${money} 🌸 𝐆𝐀𝐓𝐀𝐂𝐎𝐈𝐍𝐒`).catch(global.db.data.users[m.sender].limit -= diamante).catch(global.db.data.users[m.sender].money -= crime)
+if (media === 'text5') return conn.reply(m.chat, `《💸》Le has robado a @${randow.split`@`[0]} una cantidad de ${exp} XP`, m, {contextInfo: {mentionedJid: [randow]}}).catch(global.db.data.users[m.sender].exp += exp).catch(global.db.data.users[randow].exp -= crime) 
+} catch (e) {
+console.log(e)}}
+handler.help = ['robar'];
+handler.tags = ['xp'];
+handler.command = /^(crime|Crime)$/i
+handler.register = true
+handler.group = true
+export default handler;
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+return hours + " Hora(s) " + minutes + " Minuto(s)"}
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())];
+}
+
+global.robar = ['Robaste un Banco 🏦 y Obtuviste', 'Negociaste con el jefe de la mafia y Obtuviste :', 'tracionaste a tu amigo y obtuviste :', 'Los mafiosos te han pagado :', 'Le has robado al Administrador del Grupo', 'lograste ganar un boleto de lotería y obtuviste :', 'lograste robar a un famoso y obtuviste :'];
+global.robmal = ['La mafia te busca por no pagar tú deuda, perdiste', 'tu mejor amigo ti traicionó, perdiste', 'lograron asaltar tú casa, perdiste :']
+
 if (command == 'cofre') {
 if (global.db.data.users[m.sender].level < 9) return m.reply(`${lenguaje['nivel']()} 9 ${lenguaje['nivel2']()}`) 
 const date = global.db.data.users[m.sender].lastcofre + 86400000; //10 hs
